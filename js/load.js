acf1b8f18811f46eceb7324114fdca5773aa8bd8
lsj8687 <99197581+lsj8687@users.noreply.github.com>
2022-12-29 15:13:49 -0500

    load.js

---
+++ js/load.js
@@ -0,0 +1,8 @@
+  // reload the webpage since the map is not loaded
+  window.onload = function() {
+    if(!window.location.hash) {
+      window.location = window.location + '#loaded';
+      window.location.reload();
+    }
+  }
+  window.onload();
