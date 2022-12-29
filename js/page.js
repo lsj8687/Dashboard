576bda1a1044b1843f904f96898ed2cf23df945d
lsj8687 <99197581+lsj8687@users.noreply.github.com>
2022-12-29 15:04:46 -0500

    Location cahnged

--- js/page.js
+++ js/page.js
@@ -1,12 +1,3 @@
-// reload the webpage since the map is not loaded
-window.onload = function() {
-if(!window.location.hash) {
-    window.location = window.location + '#loaded';
-    window.location.reload();
-}
-}
-window.onload();
-
 const gra = function(min, max) {
     return Math.random() * (max - min) + min;
 }
